2b099c3e5bf4f9c8e4c332dbdb675bd7c9d29036
rhee-c31 <clhenriquesilva7@gmail.com>
2025-04-29 22:24:34 -0300

    v1.1

---
+++ _js/script.js
@@ -0,0 +1,10 @@
+
+const categories = document.querySelectorAll(".category");
+
+categories.forEach(category => {
+category.addEventListener("click", () => {
+    categories.forEach(c => c.classList.remove("active"));
+    category.classList.add("active");
+    });
+  });
+
